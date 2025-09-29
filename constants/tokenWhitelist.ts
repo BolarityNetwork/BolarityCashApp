646e92e8f4f5b25b382fdbe50d695bf23116dede
yucheng-Li <244065684@qq.com>
2025-09-29 18:19:16 +0800

    feat: add base usdc support

--- constants/tokenWhitelist.ts
+++ constants/tokenWhitelist.ts
@@ -21,6 +21,7 @@ export const TOKEN_WHITELIST: TokenWhitelistConfig = {
     ],
     'base-mainnet': [
       'native', // Base ETH
+      '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
     ],
     'arbitrum-mainnet': [
       'native', // ARB
