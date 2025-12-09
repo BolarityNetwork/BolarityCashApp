d2b2e279fc3064a299a8f52b5f6a1bdb0989b9f3
yucheng-Li <244065684@qq.com>
2025-12-09 22:37:13 +0800

    feat: useAppLock adjust

--- hooks/useAppLock.ts
+++ hooks/useAppLock.ts
@@ -5,7 +5,7 @@ import { isBiometricEnabled } from '@/utils/biometrics';
 
 export function useAppLock(appIsReady: boolean) {
   const { user } = usePersistedPrivyUser();
-  const [locked, setLocked] = useState(false);
+  const [locked, setLocked] = useState(true);
   const appState = useRef<AppStateStatus>(AppState.currentState);
   const hasInitialCheck = useRef(false);
   const isUnlocked = useRef(false);
